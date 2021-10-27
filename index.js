0e44f2617e9886d9193a9ef283530e58065c449d
Bayu <bayu@localhost.localdomain>
2021-10-27 13:17:38 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+
+// // name
+// // subject
+// // email
+// // pesan
+
+    
+// function kirim(){
+//     document.getElementById("myBtn").addEventListener("click", function(e) {
+//     e.preventDefault(); 
+    
+//     var name = document.querySelector('input[name="name"]').value;
+//     var subject = document.querySelector('input[name="subject"]').value;
+//     var email = document.querySelector('input[name="email"]').value;
+//     var pesan = document.querySelector('input[pesan="pesan"]').value;
+//     var message = "<html><br>| Nama: ${name} <br> | subject: ${subject} | <br> Email: ${email} | Pesan: ${pesan} <br></html>";
+    
+//     var token = "1915000755:AAGyTtnx1GnPGgdIU3W2LQx6ahh97Jsp_WY";
+//     var chat_id = '904419645';
+//     var url = 'https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}text=${message}&parse_mode=html';
+    
+//     var oReq = new XMLHttpRequest();
+//     oReq.open("GET", url, true);
+//     oReq.send();
+    
+//     alert("Message sent");
+//     }); }
+//     // </script>
